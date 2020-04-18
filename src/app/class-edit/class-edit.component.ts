febe78d943bba3f154367d8ffc0e673d596ea46e
longntph09052 <longntph09052@fpt.edu.vn>
2020-04-18 18:53:01 +0700

    4 files modified

--- src/app/class-edit/class-edit.component.ts
+++ src/app/class-edit/class-edit.component.ts
@@ -5,67 +5,73 @@ import { ClassService } from "../class.service";
 import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-class-edit',
-  templateUrl: './class-edit.component.html',
-  styleUrls: ['./class-edit.component.css']
+  selector: "app-class-edit",
+  templateUrl: "./class-edit.component.html",
+  styleUrls: ["./class-edit.component.css"]
 })
 export class ClassEditComponent implements OnInit {
-
   schoolid;
 
   classForm = new FormGroup({
     id: new FormControl(null),
-    schoolId: new FormControl(''),
-    name: new FormControl('',[
+    schoolId: new FormControl(""),
+    name: new FormControl("", [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(20)
     ]),
-    room_number: new FormControl('',[
-      Validators.required
-    ]),
-    total_student: new FormControl('',[
-      Validators.required
-    ]),
-    main_teacher: new FormControl('', [
+    room_number: new FormControl("", [Validators.required]),
+    total_student: new FormControl("", [Validators.required]),
+    main_teacher: new FormControl("", [
       Validators.required,
       Validators.pattern(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i)
     ])
-  })
+  });
 
-  get name() { return this.classForm.get('name'); }
-  get room_number() { return this.classForm.get('room_number'); }
-  get total_student() { return this.classForm.get('total_student'); }
-  get main_teacher() { return this.classForm.get('main_teacher'); }
+  get name() {
+    return this.classForm.get("name");
+  }
+  get room_number() {
+    return this.classForm.get("room_number");
+  }
+  get total_student() {
+    return this.classForm.get("total_student");
+  }
+  get main_teacher() {
+    return this.classForm.get("main_teacher");
+  }
 
   constructor(
     private schoolService: SchoolService,
     private route: Router,
     private ActiveRoute: ActivatedRoute,
     private classService: ClassService
-  ) { }
+  ) {}
 
   ngOnInit() {
     //lay id cua lop roi set data vao classForm
-    this.ActiveRoute.paramMap.subscribe(params =>{
-      let school = params.get('id');
-      this.schoolid=school;
-      let classId = params.get('classid');
+    this.ActiveRoute.paramMap.subscribe(params => {
+      let school = params.get("id");
+      this.schoolid = school;
+      let classId = params.get("classid");
       this.classService.getDetailClassBySchoolId(classId).subscribe(data => {
         this.classForm.setValue(data);
-      })
-    })
+      });
+    });
   }
   saveClass() {
-    if (this.classForm.value.id != null) {
-      this.classService.editClass(this.classForm.value).subscribe(data => {
-        this.route.navigate(['manager/detail-school',this.schoolid]);
-      });
+    if (this.classForm.valid) {
+      if (this.classForm.value.id != null) {
+        this.classService.editClass(this.classForm.value).subscribe(data => {
+          this.route.navigate(["manager/detail-school", this.schoolid]);
+        });
+      } else {
+        this.classService.addNewClass(this.classForm.value).subscribe(data => {
+          this.route.navigate(["manager/detail-school", this.schoolid]);
+        });
+      }
     } else {
-      this.classService.addNewClass(this.classForm.value).subscribe(data => {
-        this.route.navigate(['manager/detail-school',this.schoolid]);
-      });
+      alert("Dữ liệu nhập chưa đúnng không thể lưu !!!");
     }
   }
-
 }
--- src/app/schools-edit/schools-edit.component.ts
+++ src/app/schools-edit/schools-edit.component.ts
@@ -11,39 +11,48 @@ import { Router, ActivatedRoute } from "@angular/router";
 export class SchoolsEditComponent implements OnInit {
   schoolForm = new FormGroup({
     id: new FormControl(null),
-    name: new FormControl('',[
+    name: new FormControl("", [
       Validators.required,
       Validators.minLength(4),
       Validators.maxLength(20)
     ]),
-    logo: new FormControl('', [
+    logo: new FormControl("", [
       Validators.required,
       Validators.pattern(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i)
     ]),
-    address: new FormControl('', [
+    address: new FormControl("", [
       Validators.required,
       Validators.minLength(4)
     ]),
-    province: new FormControl('', [
+    province: new FormControl("", [
       Validators.required,
       Validators.minLength(4)
     ]),
-    president: new FormControl('', [
+    president: new FormControl("", [
       Validators.required,
       Validators.minLength(4)
     ]),
-    about: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4)
-    ])
+    about: new FormControl("", [Validators.required, Validators.minLength(4)])
   });
 
-  get name() { return this.schoolForm.get('name'); }
-  get logo() { return this.schoolForm.get('logo'); }
-  get address() { return this.schoolForm.get('address'); }
-  get province() { return this.schoolForm.get('province'); }
-  get president() { return this.schoolForm.get('president'); }
-  get about() { return this.schoolForm.get('about'); }
+  get name() {
+    return this.schoolForm.get("name");
+  }
+  get logo() {
+    return this.schoolForm.get("logo");
+  }
+  get address() {
+    return this.schoolForm.get("address");
+  }
+  get province() {
+    return this.schoolForm.get("province");
+  }
+  get president() {
+    return this.schoolForm.get("president");
+  }
+  get about() {
+    return this.schoolForm.get("about");
+  }
 
   constructor(
     private schoolSevice: SchoolService,
@@ -53,7 +62,7 @@ export class SchoolsEditComponent implements OnInit {
 
   ngOnInit() {
     this.ActiveRoute.paramMap.subscribe(params => {
-      let schoolID = params.get('id');
+      let schoolID = params.get("id");
       if (schoolID != null) {
         this.schoolSevice.getschoolById(schoolID).subscribe(data => {
           this.schoolForm.setValue(data);
@@ -62,15 +71,21 @@ export class SchoolsEditComponent implements OnInit {
     });
   }
   saveSchool() {
-    //neu da co san id thi se update id do, neu khong thi them moi
-    if (this.schoolForm.value.id != null) {
-      this.schoolSevice.editSchool(this.schoolForm.value).subscribe(data => {
-        this.route.navigate(['manager']);
-      });
+    if (this.schoolForm.valid) {
+      //neu da co san id thi se update id do, neu khong thi them moi
+      if (this.schoolForm.value.id != null) {
+        this.schoolSevice.editSchool(this.schoolForm.value).subscribe(data => {
+          this.route.navigate(["manager"]);
+        });
+      } else {
+        this.schoolSevice
+          .addNewSchool(this.schoolForm.value)
+          .subscribe(data => {
+            this.route.navigate(["manager"]);
+          });
+      }
     } else {
-      this.schoolSevice.addNewSchool(this.schoolForm.value).subscribe(data => {
-        this.route.navigate(['manager']);
-      });
+      alert("Dữ liệu nhập chưa đúnng không thể lưu !!!");
     }
   }
 }

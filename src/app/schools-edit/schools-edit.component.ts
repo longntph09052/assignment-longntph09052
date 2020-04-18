37fb449e2d5bc71a2855a06e83d679a4190c7e11
longntph09052 <longntph09052@fpt.edu.vn>
2020-04-18 18:37:48 +0700

    1 file modified

--- src/app/schools-edit/schools-edit.component.ts
+++ src/app/schools-edit/schools-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { SchoolService } from "../school.service";
 import { Router, ActivatedRoute } from "@angular/router";
 
@@ -11,13 +11,39 @@ import { Router, ActivatedRoute } from "@angular/router";
 export class SchoolsEditComponent implements OnInit {
   schoolForm = new FormGroup({
     id: new FormControl(null),
-    name: new FormControl(''),
-    logo: new FormControl(''),
-    address: new FormControl(''),
-    province: new FormControl(''),
-    president: new FormControl(''),
-    about: new FormControl('')
+    name: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ]),
+    logo: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/\.(gif|jpe?g|tiff|png|webp|bmp)$/i)
+    ]),
+    address: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    province: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    president: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ]),
+    about: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4)
+    ])
   });
+
+  get name() { return this.schoolForm.get('name'); }
+  get logo() { return this.schoolForm.get('logo'); }
+  get address() { return this.schoolForm.get('address'); }
+  get province() { return this.schoolForm.get('province'); }
+  get president() { return this.schoolForm.get('president'); }
+  get about() { return this.schoolForm.get('about'); }
 
   constructor(
     private schoolSevice: SchoolService,
@@ -36,7 +62,6 @@ export class SchoolsEditComponent implements OnInit {
     });
   }
   saveSchool() {
-    //lolol
     //neu da co san id thi se update id do, neu khong thi them moi
     if (this.schoolForm.value.id != null) {
       this.schoolSevice.editSchool(this.schoolForm.value).subscribe(data => {
